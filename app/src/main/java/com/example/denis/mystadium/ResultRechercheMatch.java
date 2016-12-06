6a0539484222f698347768b74e5e6360e997a4c4
e140677 <msgofficiel@gmail.com>
2016-12-06 09:31:44 +0100

    requestmanager ok

--- app/src/main/java/com/example/denis/mystadium/ResultRechercheMatch.java
+++ app/src/main/java/com/example/denis/mystadium/ResultRechercheMatch.java
@@ -1,13 +1,13 @@
 package com.example.denis.mystadium;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ResultRechercheMatch extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result_recherche_match);
     }
 }
